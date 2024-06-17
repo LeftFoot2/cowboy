47f817282ea79b21d7901b50902ad3e3b25cd4ae
Eli Murdock <elimurdock85@gmail.com>
2024-06-17 09:56:50 -0600

    bavk

--- src/business_logic.erl
+++ src/business_logic.erl
@@ -414,7 +414,7 @@ loc_req_test_() ->
         ]}.
 
 riakc_connection_test_() ->
-    {ok, Pid} = riakc_pb_socket:start_link("161.35.228.98", 8087),
+    {ok, Pid} = riakc_pb_socket:start_link("db.thomasjamiesonprograms.com", 8087),
     pong = riakc_pb_socket:ping(Pid).
 
 -endif.
