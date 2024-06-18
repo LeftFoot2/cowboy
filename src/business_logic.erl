fdd2d1cc608e3e2543b122f65174103e054d0a9d
Eli Murdock <elimurdock85@gmail.com>
2024-06-18 13:34:37 -0600

    no tree

--- src/business_logic.erl
+++ src/business_logic.erl
@@ -63,8 +63,7 @@ stop() -> gen_server:call(?MODULE, stop).
 %%% gen_server callbacks
 %%%===================================================================
 package_transfer_url_handler(Package_ID,Location_ID)-> 
-    gen_server:cast(?MODULE,{transfer_package,Package_ID,Location_ID}),
-    "tree".
+    gen_server:cast(?MODULE,{transfer_package,Package_ID,Location_ID}).
 
 get_loc(Package_ID) ->
     gen_server:call(?MODULE,{get_location, Package_ID}).
