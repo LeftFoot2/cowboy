cd69fc447dc2c8daf814c697a3f7ce1f83b78294
Eli Murdock <elimurdock85@gmail.com>
2024-07-17 09:58:30 -0600

    bussin

--- src/business_logic.erl
+++ src/business_logic.erl
@@ -208,7 +208,7 @@ handle_cast({put_location, _Location_ID, {<<"">>, _Longitude}}, Db_PID) ->
 handle_cast({put_location, _Location_ID, {_Latitude, <<"">>}}, Db_PID) ->
     {noreply,failed,Db_PID};
 handle_cast({put_location, Location_ID, {Latitude, Longitude}}, Db_PID) ->
-    db_api:put_location(Location_ID, [Latitude, Longitude], Db_PID),
+    db_api:put_location(Location_ID, {Latitude, Longitude}, Db_PID),
     {noreply, worked, Db_PID};
     
 handle_cast(_Msg, State) ->
